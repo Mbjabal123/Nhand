17c2724ed79af851c5c32205ce0ab719c1affab3
Mbjabal123 <benannacloset@gmail.com>
2025-05-05 19:03:41 +0530

    Nhand trial

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Handlix</h1>
+      <p>Handling Life’s Essentials Effortlessly</p>
+    </div>
+  )
+}
+
+export default App
